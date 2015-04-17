0029c5904c79c2bf9f1eb7edf2fe61fe57ffc9f8
Aaron Smith <crypticsymbols@gmail.com>
2015-04-17 09:05:47 -0400

    openshift config edits

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -3,7 +3,10 @@ def Object.const_missing(const)
     require 'redis'
 
     redis_connect = {
-      url: ENV['REDIS_URL'] || 'redis://localhost:6379/0/supermarket'
+      # url: ENV['REDIS_URL'] || 'redis://localhost:6379/0/supermarket',
+      host: ENV['OPENSHIFT_REDIS_HOST'] || 'localhost',
+      port: ENV['OPENSHIFT_REDIS_PORT'],
+      db: 0
     }
     unless ENV['REDIS_PASSWORD'].blank?
       redis_connect[:password] = ENV['REDIS_PASSWORD']
