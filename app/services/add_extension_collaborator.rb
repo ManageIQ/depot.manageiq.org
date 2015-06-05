8134d97f966daef8e11bf4352f1b9870e2229d86
Chris Vincent <c.j.vincent@gmail.com>
2015-06-05 16:28:07 -0500

    Remove commented code

--- app/services/add_extension_collaborator.rb
+++ app/services/add_extension_collaborator.rb
@@ -8,34 +8,6 @@ class AddExtensionCollaborator
     ActiveRecord::Base.transaction do
       user, account = EnsureGithubUserAndAccount.new(@github_user).process!
       Collaborator.create(user: user, resourceable: @extension)
-
-      # NOTE: This is very similar to User.find_or_create_from_github_oauth,
-      # except we're using the user object obtained from the GitHub API rather
-      # than OAuth. Also, we must save the account without OAuth fields. We
-      # ought to come back to this and DRY it up...
-
-      # account = Account.where(
-      #   username: @github_user[:login],
-      #   provider: "github"
-      # ).first_or_initialize
-
-      # if @github_user[:name].include?(" ")
-      #   split = @github_user[:name].split(" ")
-      #   first_name = split[0]
-      #   last_name = split[1]
-      # else
-      #   first_name = @github_user[:name]
-      #   last_name = nil
-      # end
-
-      # account.user ||= User.new(
-      #   first_name: first_name,
-      #   last_name: last_name,
-      #   email: @github_user[:email]
-      # )
-
-      # account.save(validate: false)
-      # account.user.save
     end
   end
 end
