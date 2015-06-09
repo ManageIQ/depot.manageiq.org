82151719f90010139bbe283a017f0ffb971b95e4
Chris Vincent <c.j.vincent@gmail.com>
2015-06-09 12:03:55 -0500

    Update spec for new collaborator object

--- spec/workers/extract_extension_collaborators_worker_spec.rb
+++ spec/workers/extract_extension_collaborators_worker_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe ExtractExtensionCollaboratorsWorker do
   let(:extension_id) { 123 }
 
-  let(:extension) { double(:extension, github_repo: "cvincent/test") }
+  let(:extension) { double(:extension, id: extension_id, github_repo: "cvincent/test") }
   let(:octokit) { double(:octokit) }
 
   let(:contributors) do
@@ -20,20 +20,20 @@ describe ExtractExtensionCollaboratorsWorker do
     allow(Rails.configuration).to receive(:octokit) { octokit }
     allow(octokit).to receive(:contributors).with("cvincent/test", nil, page: 1) { contributors }
 
-    stub_const("AddExtensionCollaborator", Class.new)
-    allow(AddExtensionCollaborator).to receive(:new) { double(:service, :process! => nil) }
+    stub_const("ExtractExtensionCollaboratorWorker", Class.new)
+    allow(ExtractExtensionCollaboratorWorker).to receive(:perform_async)
 
     allow(ExtractExtensionCollaboratorsWorker).to receive(:perform_async)
   end
 
   it "adds each collaborator returned with at least one commit" do
-    expect(AddExtensionCollaborator).to receive(:new).with(extension, contributors[0])
-    expect(AddExtensionCollaborator).to receive(:new).with(extension, contributors[2])
+    expect(ExtractExtensionCollaboratorWorker).to receive(:perform_async).with(extension_id, contributors[0][:login])
+    expect(ExtractExtensionCollaboratorWorker).to receive(:perform_async).with(extension_id, contributors[2][:login])
     subject.perform(extension_id)
   end
 
   it "skips collaborators with zero commits" do
-    expect(AddExtensionCollaborator).not_to receive(:new).with(extension, contributors[1])
+    expect(ExtractExtensionCollaboratorWorker).not_to receive(:perform_async).with(extension, contributors[1][:login])
     subject.perform(extension_id)
   end
 
