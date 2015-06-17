d812e8f798fbf047f97509bfa4cfc26c61082877
Chris Vincent <c.j.vincent@gmail.com>
2015-06-17 15:34:10 -0500

    Remove debug code

--- app/workers/extract_extension_basic_metadata_worker.rb
+++ app/workers/extract_extension_basic_metadata_worker.rb
@@ -5,15 +5,10 @@ class ExtractExtensionBasicMetadataWorker
     @extension = Extension.find(extension_id)
     repo = octokit.repo(@extension.github_repo)
 
-    @extension.assign_attributes(
+    @extension.update_attributes(
       extension_followers_count: repo[:stargazers_count],
       issues_url: "https://github.com/#{@extension.github_repo}/issues"
     )
-    puts @extension.changes
-    puts @extension.save!(validate: false).inspect
-    puts @extension.inspect
-    puts @extension.errors.inspect
-    puts Extension.find(extension_id).inspect
   end
 
   private
