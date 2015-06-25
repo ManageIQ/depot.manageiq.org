f183c40e450eec5349f5f1621f13c6d6a7b784ad
Chris Vincent <c.j.vincent@gmail.com>
2015-06-25 15:15:10 -0500

    Pull actual file contents

--- app/workers/extract_extension_version_class_worker.rb
+++ app/workers/extract_extension_version_class_worker.rb
@@ -2,6 +2,15 @@ class ExtractExtensionVersionClassWorker
   include Sidekiq::Worker
 
   def perform(version_id, path)
-    puts "EXTRACT CLASS: #{path}"
+    @version = ExtensionVersion.find(version_id)
+    @extension = version.extension
+    contents = @extension.octokit.contents(@extension.github_repo, ref: @version.version, path: path)
+
+    @version.extension_version_content_items.create(
+      name: contents[:name],
+      path: contents[:path],
+      item_type: "Class",
+      github_url: contents[:github_url]
+    )
   end
 end
--- app/workers/extract_extension_version_file_worker.rb
+++ app/workers/extract_extension_version_file_worker.rb
@@ -2,6 +2,36 @@ class ExtractExtensionVersionFileWorker
   include Sidekiq::Worker
 
   def perform(version_id, path)
-    puts "EXTRACT FILE: #{path}"
+    @version = ExtensionVersion.find(version_id)
+    @extension = version.extension
+    contents = @extension.octokit.contents(@extension.github_repo, ref: @version.version, path: path)
+    body = Base64.decode64(contents[:content])
+
+    type = if body["MiqReport"]
+      "Report"
+    elsif body["MiqPolicySet"]
+      "Policy"
+    elsif body["MiqAlert"]
+      "Alert"
+    elsif body["dialog_tabs"]
+      "Dialog"
+    elsif body["MiqWidget"]
+      "Widget"
+    else
+      "Unknown"
+    end
+
+    @version.extension_version_content_items.create(
+      name: contents[:name],
+      path: contents[:path],
+      item_type: type,
+      github_url: contents[:html_url]
+    )
+
+    # Widgets
+    # Reports (type of Widget)
+    # Policies
+    # Alerts
+    # Dialogs
   end
 end
