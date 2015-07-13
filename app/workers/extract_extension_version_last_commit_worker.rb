23bd2e292f657818892b75a02d063c618bfb95f9
Chris Vincent <c.j.vincent@gmail.com>
2015-07-13 17:03:04 -0500

    Display last commit

--- app/workers/extract_extension_version_last_commit_worker.rb
+++ app/workers/extract_extension_version_last_commit_worker.rb
@@ -10,7 +10,7 @@ class ExtractExtensionVersionLastCommitWorker
         last_commit_sha: last_commit[:sha],
         last_commit_at: last_commit[:commit][:author][:date],
         last_commit_string: last_commit[:commit][:message],
-        last_commit_url: last_commit[:url]
+        last_commit_url: last_commit[:html_url]
       )
     end
   end
