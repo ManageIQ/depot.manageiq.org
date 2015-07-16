3043701a3016e14fc3bd5cb3b9c75b7b0c5c5768
Chris Vincent <c.j.vincent@gmail.com>
2015-07-16 14:16:34 -0500

    Downcase all the tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  DEFAULT_TAGS = ["automate", "aws", "build", "data model", "dev", "policy", "policies", "events", "reports", "dashboard", "widgets", "dialogs", "vmware", "scvmm", "rhevm", "networking", "dynamic dialogs", "integration", "buttons", "spam", "configuration management", "puppet", "chef", "ansible", "soap", "rest API", "alarms", "email", "services", "catalog items", "catalog bundles", "CMDB", "rails", "ruby", "OpenStack", "Hyper-V", "RHEV", "oVirt", "vSphere", "The Foreman", "Kubernetes"]
+  DEFAULT_TAGS = ["automate", "aws", "build", "data model", "dev", "policy", "policies", "events", "reports", "dashboard", "widgets", "dialogs", "vmware", "scvmm", "rhevm", "networking", "dynamic dialogs", "integration", "buttons", "spam", "configuration management", "puppet", "chef", "ansible", "soap", "rest API", "alarms", "email", "services", "catalog items", "catalog bundles", "CMDB", "rails", "ruby", "OpenStack", "Hyper-V", "RHEV", "oVirt", "vSphere", "The Foreman", "Kubernetes"].map(&:downcase)
 
   has_many :taggings
 end
