8631be364c69b6d64ce505d964ede01069ebfaf5
Chris Vincent <c.j.vincent@gmail.com>
2015-07-28 16:36:13 -0500

    Update repo for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.4.0"
 
 set :application, "manageiq"
-set :repo_url, "git@github.com:gofullstack/extensions-depot.git"
+set :repo_url, "git@github.com:ManageIQ/depot.manageiq.org.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
