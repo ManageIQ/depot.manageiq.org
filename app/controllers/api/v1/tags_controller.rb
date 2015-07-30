b1bdfa94911a35ffd86553068a76f7d83f751801
Chris Vincent <c.j.vincent@gmail.com>
2015-07-30 11:54:35 -0500

    Always autofill default tags

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::TagsController < ApplicationController
   def index
     @tags = Tag.where("name ILIKE :search", search: "#{params['q']}%").limit(5).all
     @platforms = SupportedPlatform.where("name ILIKE :search", search: "#{params['q']}%").limit(5).all
-    render json: @tags.map(&:name) + @platforms.map(&:name)
+    @defaults = Tag::DEFAULT_TAGS.select { |t| t =~ /^#{params[:q]}/i }
+    render json: @tags.map(&:name) + @platforms.map(&:name) + @defaults
   end
 end
