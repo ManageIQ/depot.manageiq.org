cfc0306e6fa859de5cb2231e54458e6786dfd575
Chris Vincent <c.j.vincent@gmail.com>
2015-08-17 15:12:26 -0500

    Grab people who have access as well

--- app/workers/extract_extension_collaborators_worker.rb
+++ app/workers/extract_extension_collaborators_worker.rb
@@ -1,14 +1,21 @@
 class ExtractExtensionCollaboratorsWorker
   include Sidekiq::Worker
 
-  def perform(extension_id, page = 1)
+  def perform(extension_id, page = 1, from_api = :contributors)
     @extension = Extension.find(extension_id)
-    @contributors = octokit.contributors(@extension.github_repo, nil, page: page)
-    return if @contributors.empty?
 
-    process_contributors
+    if from_api == :contributors
+      @contributors = octokit.contributors(@extension.github_repo, nil, page: page)
+    else
+      @contributors = octokit.collaborators(@extension.github_repo, page: page)
+    end
 
-    self.class.perform_async(extension_id, page + 1) if @contributors.any?
+    if @contributors.any?
+      process_contributors
+      self.class.perform_async(extension_id, page + 1, from_api)
+    elsif from_api == :contributors
+      self.class.perform_async(extension_id, 1, :collaborators)
+    end
   end
 
   private
@@ -19,7 +26,7 @@ class ExtractExtensionCollaboratorsWorker
 
   def process_contributors
     @contributors.each do |c|
-      ExtractExtensionCollaboratorWorker.perform_async(@extension.id, c[:login]) if c[:contributions] > 0
+      ExtractExtensionCollaboratorWorker.perform_async(@extension.id, c[:login])
     end
   end
 end
